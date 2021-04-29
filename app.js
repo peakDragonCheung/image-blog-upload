0478642b52794f4daf07b670819a2d382c716485
Longfeng Zhang <you@e.comcls>
2021-04-29 13:19:51 +0800

    路径提交

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cors = require('@koa/cors');
 const static = require('koa-static');
 const app = new Koa();
 const upLoadImage = require('./upload.js');
-app.use(static(path.join(__dirname,'./web')));
 app.use(cors());
 app.use(koaBody({
     multipart:true, // 支持文件上传
@@ -18,14 +17,16 @@ app.use(koaBody({
       },
     }
   }));
+app.use(static(path.join(__dirname,'./web')));
+
 app.use(async ctx => {
     if(ctx.req.url === '/upload') {
         const url = await upLoadImage(ctx);
         ctx.body = url;
         return
     }
-    ctx.body = '无权访问'
 });
+
 
 console.log('开始监听..... 4040','\n','请访问 http://localhost:4040')
 app.listen(4040);
